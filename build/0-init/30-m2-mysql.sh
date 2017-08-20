3199d32f31183c18c5f5cc758acbe119120533a0
Joseph Maxwell <joseph@swiftotter.com>
2017-08-19 19:53:06 -0500

    Removing MySQL password leak

--- build/0-init/30-m2-mysql.sh
+++ build/0-init/30-m2-mysql.sh
@@ -8,9 +8,6 @@ if (( $(ps -ef | grep ${service} | wc -l) == 0 )); then
    echo "MySQL is not running, or can't be logged into, using \"root\" and no password."
    exit 125
 else
-    logvalue ${MYSQL_PASSWORD_PROMPT}
-    logvalue "mysql -u root ${MYSQL_PASSWORD_PROMPT}"
-    
     mysql -u ${MYSQL_USER} ${MYSQL_PASSWORD_PROMPT} <<-EOSQL
         DROP DATABASE IF EXISTS ${DB_NAME};
         CREATE DATABASE ${DB_NAME};
