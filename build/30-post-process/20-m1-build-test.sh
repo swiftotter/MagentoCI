ab360c7027e9b46ff7e6c00cd4e1daee9660fa23
Joseph Maxwell <joseph@swiftotter.com>
2017-08-22 14:34:39 -0500

    Update 20-m1-build-test.sh

--- build/30-post-process/20-m1-build-test.sh
+++ build/30-post-process/20-m1-build-test.sh
@@ -2,7 +2,9 @@
 
 ### 20-m1-build-test.sh: checks to ensure the build was successful.
 
-if [ ! -f ${BUILD_DIR}"/public/skin/frontend/${THEME}/*/css/*.css" ]; then
+if [[ ${THEME} != "null" ]] 
+    && [[ ! -z ${var+x} ]] 
+    && [[ ! -f ${BUILD_DIR}"/public/skin/frontend/${THEME}/*/css/*.css" ]]; then
     printf "No CSS files present for theme. Please investigate." 1>&2
     printf "BUILD ERROR" 1>&2
     exit 20
