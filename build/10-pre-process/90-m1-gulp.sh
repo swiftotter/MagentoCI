d296225409e5e64e0045b4de428b154cba272bc6
Joseph Maxwell <joseph@swiftotter.com>
2017-08-22 14:01:25 -0500

    Update 90-m1-gulp.sh

--- build/10-pre-process/90-m1-gulp.sh
+++ build/10-pre-process/90-m1-gulp.sh
@@ -2,9 +2,17 @@
 
 ### 90-gulp.sh: this file runs gulp commands on the build folder
 
-if [ ${TESTMODE} == 0 ] || [ -z ${TESTMODE+x} ]; then
-    if [ -f "${BASE}/package.json" ]; then
+if [[ ${TESTMODE} == 0 ]] || [[ -z ${TESTMODE+x} ]]; then
+    if [[ -f "${BASE}/package.json" ]]; then
         printf "Installing NPM modules"
+        npm install
+
+        printf "Gulping the files"
+        npm run gulp prod
+    fi
+    
+    if [[ -f "${BASE}/customizations/package.json" ]]; then
+        printf "Installing NPM modules (inside of customizations folder)"
         npm install
 
         printf "Gulping the files"
