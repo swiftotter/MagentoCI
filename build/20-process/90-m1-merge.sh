e53079f4fad5edcc616f39bfea977862e9bcf300
Joseph Maxwell <joseph@swiftotter.com>
2017-08-22 14:10:30 -0500

    Update 90-m1-merge.sh

--- build/20-process/90-m1-merge.sh
+++ build/20-process/90-m1-merge.sh
@@ -12,9 +12,9 @@ ${rsync_cmd} --exclude='dev/' --exclude='downloader/' --exclude='tests/' --exclu
 rm -rf ${BUILD_DIR}/public/media/
 
 ## Merging the code folders (customizations is after so it can override files from /extensions
-if [[ -d "${EXTENSIONS_DIR}" ]]; then
-    echo "${rsync_cmd} --exclude='*modman*' ${EXTENSIONS_DIR}/. ${BUILD_DIR}/public/"
-    ${rsync_cmd} --exclude='*modman*' ${EXTENSIONS_DIR}/. ${BUILD_DIR}/public/
+if [[ -d "${CHECKOUT_DIR}/extensions" ]]; then
+    echo "${rsync_cmd} --exclude='*modman*' ${CHECKOUT_DIR}/extensions/. ${BUILD_DIR}/public/"
+    ${rsync_cmd} --exclude='*modman*'${CHECKOUT_DIR}/extensions/. ${BUILD_DIR}/public/
 fi
 
 echo "${rsync_cmd} ${SCRIPT_DIR}/. ${BUILD_DIR}/scripts/"
@@ -35,31 +35,31 @@ if [[ -d "${HOME_DIR}" ]]; then
 fi
 
 ## Copying "public" composer files (those that go in the public/ folder)
-if [[ -d "${CORE_DIR}" ]]; then
-    echo "${rsync_cmd} ${CORE_DIR}/. ${BUILD_DIR}/public/"
-    ${rsync_cmd} ${CORE_DIR}/. ${BUILD_DIR}/public/
+if [[ -d "${CHECKOUT_DIR}/core" ]]; then
+    echo "${rsync_cmd} ${CHECKOUT_DIR}/core/. ${BUILD_DIR}/public/"
+    ${rsync_cmd} ${CHECKOUT_DIR}/core/. ${BUILD_DIR}/public/
 fi
 
 ## Copying "private" composer files (those that stay in vendor/)
-if [[ -f "${BASE}/phpunit.xml" ]]; then
-    ${rsync_cmd} ${BASE}/phpunit.* ${BUILD_DIR}
+if [[ -f "${CHECKOUT_DIR}/phpunit.xml" ]]; then
+    ${rsync_cmd} ${CHECKOUT_DIR}/phpunit.* ${BUILD_DIR}
 fi
 
 ## Copying "private" composer files (those that stay in vendor/)
-if [[ -f "${BASE}/composer.json" ]]; then
-    echo "${rsync_cmd} ${BASE}/composer.* ${BUILD_DIR}"
-    ${rsync_cmd} ${BASE}/composer.* ${BUILD_DIR}
-    echo "${rsync_cmd} ${BASE}/vendor/. ${BUILD_DIR}/vendor"
-    ${rsync_cmd} ${BASE}/vendor/. ${BUILD_DIR}/vendor
+if [[ -f "${CHECKOUT_DIR}/composer.json" ]]; then
+    echo "${rsync_cmd} ${CHECKOUT_DIR}/composer.* ${BUILD_DIR}"
+    ${rsync_cmd} ${CHECKOUT_DIR}/composer.* ${BUILD_DIR}
+    echo "${rsync_cmd} ${CHECKOUT_DIR}/vendor/. ${BUILD_DIR}/vendor"
+    ${rsync_cmd} ${CHECKOUT_DIR}/vendor/. ${BUILD_DIR}/vendor
 fi
 
 ## Merging the code folders (customizations is after so it can override files from /extensions)
-if [[ -d "${CUSTOMIZATIONS_DIR}" ]]; then
-    echo "${rsync_cmd} --exclude='*modman*' --exclude='vendor/' ${CUSTOMIZATIONS_DIR}/. ${BUILD_DIR}/public/"
-    ${rsync_cmd} --exclude='*modman*' --exclude='vendor/' ${CUSTOMIZATIONS_DIR}/. ${BUILD_DIR}/public/
+if [[ -d "${CHECKOUT_DIR}/customizations" ]]; then
+    echo "${rsync_cmd} --exclude='*modman*' --exclude='vendor/' ${CHECKOUT_DIR}/customizations/. ${BUILD_DIR}/public/"
+    ${rsync_cmd} --exclude='*modman*' --exclude='vendor/' ${CHECKOUT_DIR}/customizations/. ${BUILD_DIR}/public/
 fi
 
-if [[ -d "${CUSTOMIZATIONS_DIR}/vendor/" ]]; then
-    echo "${rsync_cmd} --exclude='*modman*' ${CUSTOMIZATIONS_DIR}/vendor/. ${BUILD_DIR}/vendor/"
-    ${rsync_cmd} --exclude='*modman*' ${CUSTOMIZATIONS_DIR}/vendor/. ${BUILD_DIR}/vendor/
+if [[ -d "${CHECKOUT_DIR}/customizations/vendor/" ]]; then
+    echo "${rsync_cmd} --exclude='*modman*' ${CHECKOUT_DIR}/customizations/vendor/. ${BUILD_DIR}/vendor/"
+    ${rsync_cmd} --exclude='*modman*' ${CHECKOUT_DIR}/customizations/vendor/. ${BUILD_DIR}/vendor/
 fi
