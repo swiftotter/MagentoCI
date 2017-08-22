85e8fa322ba33659221554f6212ecf64f18187e3
Joseph Maxwell <joseph@swiftotter.com>
2017-08-22 14:27:58 -0500

    M1 gulp removing custom customizations functionality

--- build/10-pre-process/90-m1-gulp.sh
+++ build/10-pre-process/90-m1-gulp.sh
@@ -14,15 +14,4 @@ if [[ ${TESTMODE} == 0 ]] || [[ -z ${TESTMODE+x} ]]; then
             npm run gulp prod
         )
     fi
-    
-    if [[ -f "${CHECKOUT_DIR}/customizations/package.json" ]]; then
-        (
-            cd ${CHECKOUT_DIR}/customizations &&
-            printf "Installing NPM modules (inside of customizations folder)" &&
-            npm install &&
-
-            printf "Gulping the files" &&
-            npm run gulp prod
-        )
-    fi
 fi
