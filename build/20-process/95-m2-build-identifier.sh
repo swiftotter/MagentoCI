97497a77e456c6bc94206de9cc5235c106404706
Joseph Maxwell <joseph@swiftotter.com>
2017-08-26 11:33:59 -0500

    Fixed typo when injecting the build identifier.

--- build/20-process/95-m2-build-identifier.sh
+++ build/20-process/95-m2-build-identifier.sh
@@ -3,5 +3,5 @@
 ### 110-m2-build-identifier.sh: exports the build number into a Magento 2 project.
 
 if [ ! -z "$BUILD_ID" -a "$BUILD_ID" != " " ]; then
-    echo "$BUILD_ID" > "${BUILD_DIR}app/design/build"
+    echo "$BUILD_ID" > "${BUILD_DIR}/app/design/build"
 fi
