40121b0fcc26003ecb461fb981461916e7e7b49b
Joseph Maxwell <joseph@swiftotter.com>
2017-09-09 13:55:16 -0500

    Update 20-m2-compile.sh

--- build/20-process/20-m2-compile.sh
+++ build/20-process/20-m2-compile.sh
@@ -12,7 +12,7 @@ mysql -u root ${MYSQL_PASSWORD_PROMPT} ${DB_NAME} <<-EOSQL
   INSERT INTO core_config_data (config_id, scope, scope_id, path, value) VALUES (null, 'default', 0, 'dev/css/minify_files', '1');
 EOSQL
 
-$MAGENTO_CMD deploy:mode:set production
+$MAGENTO_CMD deploy:mode:set production --skip-compilation
 $MAGENTO_CMD setup:upgrade
 $MAGENTO_CMD setup:di:compile
 $MAGENTO_CMD setup:static-content:deploy
