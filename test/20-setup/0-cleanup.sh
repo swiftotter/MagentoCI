6cc3e6c4d311e237a8b2aedd6b5c4d5aeb1ce280
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 09:46:01 -0500

    Update 0-cleanup.sh

--- test/20-setup/0-cleanup.sh
+++ test/20-setup/0-cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ### 0-cleanup.sh: This file sets up the variables needed for deployment
+logvalue ${MAGENTO}"/var/cache"
+logvalue ${OUTPUT_DIR}
+logvalue ${LOCAL_CONFIG}
+
 
 rm -rf ${MAGENTO}"/var/cache"
 rm -rf ${OUTPUT_DIR}
