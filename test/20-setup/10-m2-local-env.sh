6169a7296939e2bb42e546552241c4edeec56851
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 10:52:44 -0500

    Adding install-config-mysql.php

--- test/20-setup/10-m2-local-env.sh
+++ test/20-setup/10-m2-local-env.sh
@@ -8,3 +8,11 @@ sed -i -e "s/--table-prefix--/${TABLE_PREFIX}/g" "${LOCAL_CONFIG}"
 sed -i -e "s/--username--/${MYSQL_USER}/g" "${LOCAL_CONFIG}"
 sed -i -e "s/--password--/${MYSQL_PASSWORD}/g" "${LOCAL_CONFIG}"
 sed -i -e "s/--database--/${MYSQL_DB_NAME}/g" "${LOCAL_CONFIG}"
+
+
+cp "${TEST_SCRIPT_DIR}/files/install-config-mysql.php" "${BUILD}/dev/tests/integration/etc"
+sed -i -e "s/--crypt-key--/${CRYPT_KEY}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--table-prefix--/${TABLE_PREFIX}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--username--/${MYSQL_USER}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--password--/${MYSQL_PASSWORD}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--database--/${MYSQL_DB_NAME}/g" "${LOCAL_CONFIG}"
