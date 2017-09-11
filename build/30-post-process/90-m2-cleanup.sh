7db45bcdc1ca63feb29d0508ba5e5a5d4814f8bb
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 11:08:01 -0500

    Update 90-m2-cleanup.sh

--- build/30-post-process/90-m2-cleanup.sh
+++ build/30-post-process/90-m2-cleanup.sh
@@ -2,7 +2,7 @@
 
 ### 150-m2-cleanup.sh: removes the database for Magento 2
 
-logvalue "Cleaning up MySQL"
+logvalue "Cleaning up MySQL: DROP DATABASE IF EXISTS ${DB_NAME};"
 
 mysql -u root ${MYSQL_PASSWORD_PROMPT} <<-EOSQL
     DROP DATABASE IF EXISTS ${DB_NAME};
