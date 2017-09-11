f92f8986a70a1bc9479c044df922e6e9eee43d98
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 09:49:42 -0500

    Update 10-phpunit.sh

--- test/30-unit/10-phpunit.sh
+++ test/30-unit/10-phpunit.sh
@@ -2,6 +2,8 @@
 
 ### 10-local-xml.sh: This file sets up the variables needed for deployment
 
+logvalue ${PHPUNIT_DIR}
+
 if [ -d ${PHPUNIT_DIR} ]; then
     ${php} ${PHPUNIT_BIN} --configuration ${PHPUNIT_CONFIG_FILE} --stderr --log-junit ${OUTPUT_DIR}/phpunit.xml
 fi
