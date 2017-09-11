5c6556525e1bb5d650d0730ffb4e5d8ad2b9b1cf
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 10:07:16 -0500

    Create 10-m2-local-env.sh

---
+++ test/20-setup/10-m2-local-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### 10-m2-local-env.sh: This file sets up the variables needed for deployment
+
+cp "${TEST_SCRIPT_DIR}/files/env.php" "${LOCAL_CONFIG}"
+sed -i -e "s/--crypt-key--/${CRYPT_KEY}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--table-prefix--/${TABLE_PREFIX}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--username--/${MYSQL_USER}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--password--/${MYSQL_PASSWORD}/g" "${LOCAL_CONFIG}"
+sed -i -e "s/--database--/${MYSQL_DB_NAME}/g" "${LOCAL_CONFIG}"
