f867fc3eec84f4379b093917e776ace9c4b313af
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 12:16:02 -0500

    Update and rename 10-phpunit-integration.sh to 10-m2-integration-tests.sh

--- test/30-unit/10-m2-integration-tests.sh
+++ test/30-unit/10-m2-integration-tests.sh
@@ -3,5 +3,5 @@
 ### 10-local-xml.sh: This file sets up the variables needed for deployment
 
 if [ -d ${PHPUNIT_DIR} ] && [ -f ${BUILD}/dev/tests/integration/phpunit.xml ]; then
-    (cd ${BUILD}/dev/tests/integration && ${php} ${PHPUNIT_BIN} --stderr --log-junit ${OUTPUT_DIR}/phpunit.xml)
+    (cd ${BUILD}/dev/tests/integration && ${php} ${PHPUNIT_BIN} --stderr --log-junit ${OUTPUT_DIR}/phpunit-integration.xml)
 fi
