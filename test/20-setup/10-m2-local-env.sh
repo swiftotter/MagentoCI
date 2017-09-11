124452740e758942144259a505a1117e02771a6d
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 10:57:18 -0500

    Update 10-m2-local-env.sh

--- test/20-setup/10-m2-local-env.sh
+++ test/20-setup/10-m2-local-env.sh
@@ -9,10 +9,10 @@ sed -i -e "s/--username--/${MYSQL_USER}/g" "${LOCAL_CONFIG}"
 sed -i -e "s/--password--/${MYSQL_PASSWORD}/g" "${LOCAL_CONFIG}"
 sed -i -e "s/--database--/${MYSQL_DB_NAME}/g" "${LOCAL_CONFIG}"
 
-
-cp "${TEST_SCRIPT_DIR}/files/install-config-mysql.php" "${BUILD}/dev/tests/integration/etc"
-sed -i -e "s/--crypt-key--/${CRYPT_KEY}/g" "${LOCAL_CONFIG}"
-sed -i -e "s/--table-prefix--/${TABLE_PREFIX}/g" "${LOCAL_CONFIG}"
-sed -i -e "s/--username--/${MYSQL_USER}/g" "${LOCAL_CONFIG}"
-sed -i -e "s/--password--/${MYSQL_PASSWORD}/g" "${LOCAL_CONFIG}"
-sed -i -e "s/--database--/${MYSQL_DB_NAME}/g" "${LOCAL_CONFIG}"
+TEST_CONFIG_FILE=${BUILD}/dev/tests/integration/etc/install-config-mysql.php
+cp "${TEST_SCRIPT_DIR}/files/install-config-mysql.php" "${TEST_CONFIG_FILE}"
+sed -i -e "s/--crypt-key--/${CRYPT_KEY}/g" "${TEST_CONFIG_FILE}"
+sed -i -e "s/--table-prefix--/${TABLE_PREFIX}/g" "${TEST_CONFIG_FILE}"
+sed -i -e "s/--username--/${MYSQL_USER}/g" "${TEST_CONFIG_FILE}"
+sed -i -e "s/--password--/${MYSQL_PASSWORD}/g" "${TEST_CONFIG_FILE}"
+sed -i -e "s/--database--/${MYSQL_DB_NAME}/g" "${TEST_CONFIG_FILE}"
