d4898bd6f73197036af23b127a061d1aa0fcb773
Joseph Maxwell <joseph@swiftotter.com>
2017-09-13 20:11:53 -0500

    Update 99-warm-opcache.sh

--- deploy/40-release/99-warm-opcache.sh
+++ deploy/40-release/99-warm-opcache.sh
@@ -2,8 +2,9 @@
 
 ### 99-warm-opcache.sh: this triggers a curl call to the HTTP path
 
-logvalue "Curling the website to warm caches."
+logvalue "Curling the website to warm caches: ${HTTP_PATH}"
 
 if [ ${#HTTP_PATH} -ge 1 ]; then
+    logvalue "Executing cURL: ${HTTP_PATH}"
     curl -I ${HTTP_PATH}
 fi
