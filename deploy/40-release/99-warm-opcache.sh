2a6665019ca8c275bb3c47c8aad60f2cb4efa822
Joseph Maxwell <joseph@swiftotter.com>
2017-09-15 12:17:54 -0500

    Adding opcache warming

--- deploy/40-release/99-warm-opcache.sh
+++ deploy/40-release/99-warm-opcache.sh
@@ -4,6 +4,8 @@
 
 logvalue "Curling the website to warm caches: ${HTTP_PATH}"
 
+find ${OUTPUT_DIR} -type f -print0 | xargs -0 -P128 -n1 md5sum
+
 #if [ ${#HTTP_PATH} -ge 1 ]; then
 #    logvalue "Executing cURL: ${HTTP_PATH}"
 #    curl -I ${HTTP_PATH}
