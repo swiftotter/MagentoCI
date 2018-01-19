830dea800df6bfe29811298fcf1f4dce3b5117a9
Joseph Maxwell <joseph@swiftotter.com>
2018-01-19 13:47:56 -0600

    Removing permissions setting for now

--- deploy/30-process/90-m2-set-permissions.sh
+++ deploy/30-process/90-m2-set-permissions.sh
@@ -3,11 +3,11 @@
 ### 90-m2-set-permissions.sh: sets the permissions for Magento 2 projects
 
 logvalue "Setting directory permissions:"
-find ${RELEASE_DIR}/var ${RELEASE_DIR}/vendor ${RELEASE_DIR}/pub/static ${RELEASE_DIR}/pub/media ${RELEASE_DIR}/app/etc -type f -exec chmod u+w {} \;
+#find ${RELEASE_DIR}/var ${RELEASE_DIR}/vendor ${RELEASE_DIR}/pub/static ${RELEASE_DIR}/pub/media ${RELEASE_DIR}/app/etc -type f -exec chmod u+w {} \;
 
 logvalue "Setting file permissions:"
-find ${RELEASE_DIR}/var ${RELEASE_DIR}/vendor ${RELEASE_DIR}/pub/static ${RELEASE_DIR}/pub/media ${RELEASE_DIR}/app/etc -type d -exec chmod u+w {} \;
+#find ${RELEASE_DIR}/var ${RELEASE_DIR}/vendor ${RELEASE_DIR}/pub/static ${RELEASE_DIR}/pub/media ${RELEASE_DIR}/app/etc -type d -exec chmod u+w {} \;
 
 logvalue "Setting directory permissions:"
-chmod u+x ${RELEASE_DIR}/bin/magento
-chmod -R 775 ${RELEASE_DIR}/var
+#chmod u+x ${RELEASE_DIR}/bin/magento
+#chmod -R 775 ${RELEASE_DIR}/var
