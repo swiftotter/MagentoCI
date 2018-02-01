4208e1b52fd206f81daa27583cf56c42d7c39e80
Joseph Maxwell <joseph@swiftotter.com>
2018-02-01 11:31:40 -0600

    Update 50-m2-gulp.sh

--- build/20-process/50-m2-gulp.sh
+++ build/20-process/50-m2-gulp.sh
@@ -2,6 +2,14 @@
 
 ### 50-m2-gulp.sh: this file runs the Snowdog Apps gulp commands
 
+if [ -f "${CHECKOUT_DIR}/package.json" ]; then
+    (cd "${BUILD_DIR}/"  \
+        && npm install \
+        && npm run prod \
+    )
+fi
+
+
 if [ ! -z "$THEME" -a "$THEME" != " " ]; then
 
     (cd "${BUILD_DIR}/vendor/snowdog/frontools/"  \
@@ -11,11 +19,4 @@ if [ ! -z "$THEME" -a "$THEME" != " " ]; then
         && gulp styles --prod --disableMaps --theme ${THEME} --gulpfile gulpfile.js \
     )
 
-fi
-
-if [ -f "${CHECKOUT_DIR}/package.json" ]; then
-    (cd "${BUILD_DIR}/"  \
-        && npm install \
-        && npm run prod \
-    )
 fi
