0d3627e4e169bca0ce679ad3c2ea5ff12e4dc45f
Joseph Maxwell <joseph@swiftotter.com>
2018-02-13 12:54:55 -0600

    Update 20-m2-unit-tests.sh

--- test/30-unit/20-m2-unit-tests.sh
+++ test/30-unit/20-m2-unit-tests.sh
@@ -3,5 +3,5 @@
 ### 10-local-xml.sh: This file sets up the variables needed for deployment
 
 if [ -d ${PHPUNIT_DIR} ] && [ -f ${BUILD}/dev/tests/unit/phpunit.xml ]; then
-    (cd ${BUILD}/dev/tests/unit && ${php} -d memory_limit=512M ${PHPUNIT_BIN} --stderr --log-junit ${OUTPUT_DIR}/phpunit-unit.xml)
+    (cd ${BUILD}/dev/tests/unit && ${PHP} -d memory_limit=512M ${PHPUNIT_BIN} --stderr --log-junit ${OUTPUT_DIR}/phpunit-unit.xml)
 fi
