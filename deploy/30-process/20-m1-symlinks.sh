31138c39987ad4687e6012031d2dfa8bc7e97f00
Joseph Maxwell <joseph@swiftotter.com>
2018-04-11 12:51:58 -0500

    Adding symlinks from home directory

--- deploy/30-process/20-m1-symlinks.sh
+++ deploy/30-process/20-m1-symlinks.sh
@@ -30,3 +30,11 @@ for file in `find ${LINK_DIR} -maxdepth 1 -type f -name "robots*" | xargs -I {} 
 do
     ln -s ${LINK_DIR}/$file ${RELEASE_DIR}/public/$file
 done
+
+## LINK ALL FILES IN link/home DIR:
+if [ -d "${LINK_DIR}/home" ]; then
+    for f in $(ls -d ${LINK_DIR}/home/*); 
+    do 
+        ln -s $f ${RELEASE_DIR}/public/;
+    done
+fi
