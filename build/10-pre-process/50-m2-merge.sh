23863989eaba263ae3cba05e68201c04706c15d1
Joseph Maxwell <joseph@swiftotter.com>
2018-05-25 13:10:36 -0500

    Adding override folder capabilities

--- build/10-pre-process/50-m2-merge.sh
+++ build/10-pre-process/50-m2-merge.sh
@@ -7,6 +7,11 @@ rsync_cmd="rsync --recursive --copy-links --specials --exclude='.git/' --exclude
 ${rsync_cmd} --exclude='var/' --exclude='*.sample' --exclude='LICENSE.*' ${CHECKOUT_DIR}/. ${BUILD_DIR}/
 ${rsync_cmd} ${CHECKOUT_DIR}/home/* ${BUILD_DIR}/pub
 
+if [[ -d "${CHECKOUT_DIR}/home/override" ]]; then
+  ${rsync_cmd} ${CHECKOUT_DIR}/home/override/* ${BUILD_DIR}
+  rm -rf ${BUILD_DIR}/pub/override
+fi
+
 
 mkdir -p ${BUILD_DIR}/scripts
 ${rsync_cmd} ${SCRIPT_DIR}/. ${BUILD_DIR}/scripts
