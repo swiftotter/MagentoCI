a054e2c5cec9069710aa5bd4e348a6c706ac4e8b
Joseph Maxwell <joseph@swiftotter.com>
2018-06-12 14:41:58 -0500

    Update 20-m1-composer.sh

--- build/10-pre-process/20-m1-composer.sh
+++ build/10-pre-process/20-m1-composer.sh
@@ -8,6 +8,6 @@ if [ -f "${CHECKOUT_DIR}/composer.json" ]; then
     logvalue "Executing composer install"
     (
           cd ${CHECKOUT_DIR} && $PHP /usr/local/bin/composer install --ignore-platform-reqs --no-interaction --no-plugins --no-dev  --no-progress --no-suggest --prefer-dist --optimize-autoloader &&
-          $PHP /usr/local/bin/composer run-script post-install-cmd -vvv -- --redeploy
+          $PHP /usr/local/bin/composer run-script post-install-cmd -vvv -- --redeploy -n
     )
 fi
