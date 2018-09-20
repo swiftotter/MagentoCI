86bf49b53037a7976c9f677bfd486532ccddf033
Joseph Maxwell <joseph@swiftotter.com>
2018-09-20 14:44:45 -0500

    Update 40-m1-magento-download.sh

--- build/10-pre-process/40-m1-magento-download.sh
+++ build/10-pre-process/40-m1-magento-download.sh
@@ -4,7 +4,7 @@
 
 if [ ! -f "${MAGENTO_STAGING}/index.php" ]; then
     printf "Downloading Magento"
-    git clone https://github.com/OpenMage/magento-mirror.git ${MAGENTO_STAGING}
+    git clone https://github.com/SwiftOtter/magento-mirror.git ${MAGENTO_STAGING}
 else
     echo "Magento is already installed, we are not downloading it again."
 fi
