9188847284420686de750198fe268a62ec8dc82a
Chris Meyers <chris.meyers@swiftotter.com>
2018-09-26 08:22:30 -0500

    Keep x builds after deploy.

--- deploy/50-cleanup/10-clear-old-builds.sh
+++ deploy/50-cleanup/10-clear-old-builds.sh
@@ -12,12 +12,20 @@ if [ -f "${BUILD_ID}.tar.gz" ]; then
     rm -f "${BUILD_ID}.tar.gz"
 fi
 
-PREVIOUS=`expr ${BUILD} - 1`
-shopt -s nullglob
-for FILE in ${RELEASE_PARENT_DIR}/*-*
+NUM=0
+PREVIOUS_BUILDS=2
+for FILE in `ls -d -- ${RELEASE_PARENT_DIR}/*-* | sort -t- -k2 -r`
 do
-    if [[ ${FILE} != *"-${BUILD}"* ]] && [[ ${FILE} != *"-${PREVIOUS}"* ]]; then
-        printf "DELETING ${FILE}\n"
-        rm -rf ${FILE}
+    if [[ ${FILE} != *"-${BUILD}"* ]]; then
+        let "NUM=NUM+1"
+        if [[ $NUM > $PREVIOUS_BUILDS ]]; then
+            printf "DELETING ${FILE}\n"
+            rm -rf ${FILE}
+        else
+            IFS='-' read -ra BUILD_ARR <<< "$FILE"
+            printf "Keeping recent build ${BUILD_ARR[1]}\n"
+        fi
+    else
+        printf "Keeping current build ${BUILD}\n"
     fi
 done
