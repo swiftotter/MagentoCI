cc2cb6772dd4324cbe663b1201005e207c7b331e
Joseph Maxwell <joseph@swiftotter.com>
2019-02-02 15:55:05 -0600

    Update 30-m2-mysql.sh

--- build/0-init/30-m2-mysql.sh
+++ build/0-init/30-m2-mysql.sh
@@ -4,12 +4,6 @@
 
 service="mysql"
 
-if (( $(ps -ef | grep ${service} | wc -l) == 0 )); then
-   echo "MySQL is not running, or can't be logged into, using \"root\" and no password."
-   exit 125
-else
-    ${MYSQL} -u ${MYSQL_USER} ${MYSQL_PASSWORD_PROMPT} --connect-expired-password <<-EOSQL
-        DROP DATABASE IF EXISTS ${DB_NAME};
-        CREATE DATABASE ${DB_NAME};
-EOSQL
-fi
+ ${MYSQL} -u ${MYSQL_USER} ${MYSQL_PASSWORD_PROMPT} --connect-expired-password <<-EOSQL
+     DROP DATABASE IF EXISTS ${DB_NAME};
+     CREATE DATABASE ${DB_NAME};
