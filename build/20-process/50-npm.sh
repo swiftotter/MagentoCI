8a95bb8ec0b0a2358ae11d3d246a72ca78ff4f67
Joseph Maxwell <joseph@swiftotter.com>
2019-02-04 14:52:05 -0600

    Update 50-npm.sh

--- build/20-process/50-npm.sh
+++ build/20-process/50-npm.sh
@@ -3,7 +3,8 @@
 ### 50-m2-gulp.sh: this file runs the Snowdog Apps gulp commands
 
 if [ -f "${CHECKOUT_DIR}/package.json" ]; then
-    (cd "${CHECKOUT_DIR}/"  \
+    (cd "${CHECKOUT_DIR}/" \
+        && ls -alh \
         && yarn install \
         && yarn run prod \
     )
