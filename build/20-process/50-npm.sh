dee2ea44e6100a982293648e10a5593ba404e0d9
Jesse Maxwell <bassplayer7@users.noreply.github.com>
2019-02-05 08:37:37 -0600

    Adding code to see contents

--- build/20-process/50-npm.sh
+++ build/20-process/50-npm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### 50-m2-gulp.sh: this file runs the Snowdog Apps gulp commands
-
+ls -alh "${BUILD_DIR}/"
 if [ -f "${BUILD_DIR}/package.json" ]; then
     (cd "${BUILD_DIR}/" \
         && ls -alh \
