eee2fb8b6fea9c96a7472b0096efc2046e25785b
Joseph Maxwell <joseph@swiftotter.com>
2019-07-01 14:06:27 -0500

    Update 20-m2-compile.sh

--- build/20-process/20-m2-compile.sh
+++ build/20-process/20-m2-compile.sh
@@ -16,4 +16,4 @@ $MAGENTO_CMD deploy:mode:set production --skip-compilation
 $MAGENTO_CMD setup:upgrade
 $MAGENTO_CMD setup:di:compile
 HTTPS="on"
-$MAGENTO_CMD setup:static-content:deploy --content-version=${BUILD_ID} --jobs=2
+$MAGENTO_CMD setup:static-content:deploy --content-version=${BUILD_ID} --jobs=1
