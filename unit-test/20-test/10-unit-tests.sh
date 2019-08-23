10e6500e8cc52ee254c59d792ad3640c2ac4c9cd
Joseph Maxwell <joseph@swiftotter.com>
2019-08-23 13:00:08 -0500

    Update 10-unit-tests.sh

--- unit-test/20-test/10-unit-tests.sh
+++ unit-test/20-test/10-unit-tests.sh
@@ -7,5 +7,6 @@ logvalue ${CHECKOUT_DIR}/dev/tests/unit/phpunit.xml
 
 if [ -d ${PHPUNIT_DIR} ] && [ -f ${CHECKOUT_DIR}/dev/tests/unit/phpunit.xml ]; then
     logvalue "Running PHPUNIT"
+    ls -alh ${CHECKOUT_DIR}/dev/tests/unit/
     ${PHP} -dmemory_limit=512m ${PHPUNIT_BIN} --stderr --log-junit ${CHECKOUT_DIR}/phpunit-unit.xml --configuration ${CHECKOUT_DIR}/dev/tests/unit/phpunit.xml
 fi
