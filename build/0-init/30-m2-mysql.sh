8fe8f70a011d9bcc8d7dd573b9ca77a92e24396a
Joseph Maxwell <joseph@swiftotter.com>
2019-10-21 22:06:39 +0200

    Delete 30-m2-mysql.sh

--- build/0-init/30-m2-mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### 30-m2-mysql.sh: this file mysql for the build process
-
-MYSQL_HOST="mysql"
-
-# ${MYSQL} -u ${MYSQL_USER} ${MYSQL_PASSWORD_PROMPT} --connect-expired-password <<-EOSQL
-#     DROP DATABASE IF EXISTS ${DB_NAME};
-#     CREATE DATABASE ${DB_NAME};
