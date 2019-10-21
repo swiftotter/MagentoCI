3e62282a1ba8e55a4a74cc8bc114f5ccffbe2158
Joseph Maxwell <joseph@swiftotter.com>
2019-10-21 22:05:49 +0200

    Update 30-m2-mysql.sh

--- build/0-init/30-m2-mysql.sh
+++ build/0-init/30-m2-mysql.sh
@@ -2,7 +2,7 @@
 
 ### 30-m2-mysql.sh: this file mysql for the build process
 
-service="mysql"
+MYSQL_HOST="mysql"
 
 # ${MYSQL} -u ${MYSQL_USER} ${MYSQL_PASSWORD_PROMPT} --connect-expired-password <<-EOSQL
 #     DROP DATABASE IF EXISTS ${DB_NAME};
