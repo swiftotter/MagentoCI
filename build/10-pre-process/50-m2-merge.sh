24318003d567bb65d4b60373136a30812c505ebc
Joseph Maxwell <joseph@swiftotter.com>
2019-12-03 15:57:20 -0500

    Update 50-m2-merge.sh

--- build/10-pre-process/50-m2-merge.sh
+++ build/10-pre-process/50-m2-merge.sh
@@ -6,6 +6,12 @@ rsync_cmd="rsync --recursive --copy-links --specials --exclude='.git/' --exclude
 
 ${rsync_cmd} --exclude='var/' --exclude='*.sample' --exclude='LICENSE.*' ${CHECKOUT_DIR}/. ${BUILD_DIR}/
 ${rsync_cmd} ${CHECKOUT_DIR}/home/* ${BUILD_DIR}/pub
+rm -rf ${BUILD_DIR}/pub/override
+
+if [ -d ${CHECKOUT_DIR}/home/override ]; then
+    logvalue "Applying overrides"
+    ${rsync_cmd} -v ${CHECKOUT_DIR}/home/override/* ${BUILD_DIR}
+fi
 
 mkdir -p ${BUILD_DIR}/scripts
 ${rsync_cmd} ${SCRIPT_DIR}/. ${BUILD_DIR}/scripts
