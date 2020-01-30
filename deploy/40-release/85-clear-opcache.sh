7a8b4ff3d2b7fdd84e0ede69d7a3eb3d489895e8
Joseph Maxwell <joseph@swiftotter.com>
2020-01-30 15:29:11 -0600

    Update 85-clear-opcache.sh

--- deploy/40-release/85-clear-opcache.sh
+++ deploy/40-release/85-clear-opcache.sh
@@ -7,3 +7,8 @@ if [[ -d "${RELEASE_DIR}/vendor/bin/cachetool" ]]; then
     logvalue "Clearing caches..."
     (cd ${RELEASE_DIR} && ${PHP} vendor/gordalina/cachetool/bin/cachetool opcache:reset)
 fi
+
+if [[ -f "/usr/share/stratus/cli" ]]; then
+    logvalue "Clearing Stratus caches..."
+    /usr/share/stratus/cli cache.all.clear
+fi
