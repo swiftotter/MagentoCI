d37d02d616d4f258f0f18772f4744b0ad7c944a2
Joseph Maxwell <joseph@swiftotter.com>
2020-02-18 13:15:06 -0600

    Update php.sh

--- utilities/php.sh
+++ utilities/php.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-if [ -x "$(command -v php73)" ]; then
+if [ -n $PHP_EXEC ]; then
+  PHP=$PHP_EXEC
+elif [ -x "$(command -v php73)" ]; then
   PHP="php73"
 elif [ -x "$(command -v php72)" ]; then
   PHP="php72"
 elif [ -x "$(command -v php71)" ]; then
-  PHP="php71";
+  PHP="php71"
 elif [ -x "$(command -v php70)" ]; then
-  PHP="php70";
+  PHP="php70"
 else
-  PHP="php";
+  PHP="php"
 fi
 
-echo $PHP
+echo "PHP executable is: " ${PHP}
 
 export PHP=$PHP
